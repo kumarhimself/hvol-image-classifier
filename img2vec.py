b038b536b69494176d262000943e077dc35f1520
Ayush Kumar <ayush.k@outlook.com>
2025-04-23 09:49:44 +0000

    Created CIFAR100 classifier.

--- img2vec.py
+++ img2vec.py
@@ -6,10 +6,10 @@ model = models.resnet50(pretrained=True)
 model = torch.nn.Sequential(*(list(model.children())[:-1]))  # remove final classification layer
 _ = model.eval()
 
-def img2veimg2vecc(img_tensor):
-  if img_tensor.dim() == 3:
-    img_tensor = img_tensor.unsqueeze(0)  # add batch dimension
-  with torch.no_grad():
-    vec = model(img_tensor).squeeze()
+def img2vec(img_tensor):
+    if img_tensor.dim() == 3:
+        img_tensor = img_tensor.unsqueeze(0)  # add batch dimension
+    with torch.no_grad():
+        vec = model(img_tensor).squeeze()
 
-  return vec
+    return vec
